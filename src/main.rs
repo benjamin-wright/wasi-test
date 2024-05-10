83e56d75fa64cfb11907fb05282ff85c80e3ba67
Ben Wright <bwright@roku.com>
2024-05-10 14:49:47 +0100

    first go at refactoring worked

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,32 @@
-use std::net::SocketAddr;
+use std::pin::Pin;
 
-use hyper::{service::{make_service_fn, service_fn}, Body, Error, Request, Response, Server};
+use hyper::{Body, Error, Request, Response};
 
 mod server;
 
-use server::wait_for_kill::wait_for_kill_signal;
+#[derive(Clone)]
+struct HandlerImpl;
 
-
-async fn handler(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    println!("request: {:?} {:?}!", req.method(), req.uri());
-    match req.uri().path() {
-        "/games" => games(req).await,
-        _ => not_found().await,
+impl server::Handler for HandlerImpl {
+    fn handle(&self, req: Request<Body>) -> Pin<Box<dyn std::future::Future<Output = Result<Response<Body>, Error>> + Send>> {
+        Box::pin(async move {
+            println!("request: {:?} {:?}!", req.method(), req.uri());
+            match req.uri().path() {
+                "/games" => Ok(Response::new(Body::from("These are your games"))),
+                _ => Ok(Response::builder()
+                    .status(404)
+                    .body(Body::from("Not Found"))
+                    .unwrap()),
+            }
+        })
     }
-}
-
-async fn games(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let response = Response::builder()
-        .status(200)
-        .body(Body::from("These are your games"))
-        .unwrap();
-
-    Ok(response)
-}
-
-async fn not_found() -> Result<Response<Body>, hyper::Error> {
-    let response = Response::builder()
-        .status(404)
-        .body(Body::from("Not Found"))
-        .unwrap();
-
-    Ok(response)
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    
-    let server = Server::bind(&addr).serve(make_service_fn(|_| async {
-        Ok::<_, Error>(service_fn(handler))
-    }));
-    println!("Listening on http://{}", addr);
-
-    let graceful = server.with_graceful_shutdown(wait_for_kill_signal());
-
-    if let Err(err) = graceful.await {
-        println!("Error shutting down server: {:?}", err);
-    } else {
-        println!("Server shut down gracefully");
+    if let Err(err) = server::start(8080, HandlerImpl{}).await {
+        println!("Error starting server: {:?}", err);
+        return Err(err);
     }
 
     Ok(())
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,42 +1,33 @@
 use std::future::Future;
 use std::net::SocketAddr;
-use hyper::server::conn::{AddrIncoming, Http};
-use hyper::{Error, Request, Server};
+use std::pin::Pin;
+use hyper::{Error, Request, Response, Body, Server};
 
 use hyper::service::{make_service_fn, service_fn};
 
-pub mod wait_for_kill;
+mod wait_for_kill;
 
-// async fn handle(req: Request<hyper::Body>) -> Result<hyper::Response<hyper::Body>, hyper::Error> {
-//     println!("request: {:?} {:?}!", req.method(), req.uri());
-//     match req.uri().path() {
-//         "/games" => Ok(hyper::Response::new(hyper::Body::from("These are your games"))),
-//         "/kill" => {
-//             tokio::spawn(async {
-//                 tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-//                 std::process::exit(0);
-//             });
-//             Ok(hyper::Response::new(hyper::Body::from("Server is shutting down")))
-//         },
-//         _ => Ok(hyper::Response::builder()
-//             .status(404)
-//             .body(hyper::Body::from("Not Found"))
-//             .unwrap()),
-//     }
-// }
-
-pub trait Handler {
-    async fn handle(&self, req: Request<hyper::Body>) -> Result<hyper::Response<hyper::Body>, hyper::Error>;
+pub trait Handler: Send {
+    fn handle(&self, req: Request<hyper::Body>) -> Pin<Box<dyn Future<Output = Result<Response<Body>, Error>> + Send>>;
 }
 
-pub async fn start(port: u16, handler: impl Handler + Send + Sync) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
+pub async fn start(port: u16, handler: impl Handler + Send + Sync + Clone + 'static) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
 {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    let server = Server::bind(&addr).serve(make_service_fn(|_| async {
-        Ok::<_, Error>(service_fn(|_req: Request<hyper::Body>| async {
-            handler.handle(_req).await
-        }))
-    }));
+    let make_svc = make_service_fn(move |_| {
+        let handler = handler.clone();
+        async {
+            Ok::<_, Error>(service_fn(move |req| {
+                let handler = handler.clone();
+                async move {
+                    let res = handler.handle(req);
+                    let res = res.await?;
+                    Ok::<Response<Body>, Error>(res)
+                }
+            }))
+        }
+    });
+    let server = Server::bind(&addr).serve(make_svc);
     println!("Listening on http://{}", addr);
 
     let graceful = server.with_graceful_shutdown(wait_for_kill::wait_for_kill_signal());
